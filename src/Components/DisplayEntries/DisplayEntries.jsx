a3b2d05e3573f3abbe57e6549e212909fbe49bbf
Mdbshero <mdbshero@gmail.com>
2022-07-13 13:55:21 -0500

    added Entry Table Component

---
+++ src/Components/DisplayEntries/DisplayEntries.jsx
@@ -0,0 +1,29 @@
+
+
+const DisplayEntries = (props) => {
+    const entries = props.entries;
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>Entry Number</th>
+          <th>Weight</th>
+          <th>Date</th>
+        </tr>
+      </thead>
+      <tbody>
+        {entries.map((entry, index) => {
+          return (
+            <tr>
+              <td>{index + 1}</td>
+              <td>{entry.weight}</td>
+              <td>{entry.date}</td>
+            </tr>
+          );
+        })}
+      </tbody>
+    </table>
+  );
+};
+
+export default DisplayEntries;
